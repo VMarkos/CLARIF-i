4e0a3aa2e48efa3837ceef0169498f4eb0ecc09f
bill <vassileiosmarkos@gmail.com>
2025-05-16 11:15:42 +0300

    MInor non-fixes

--- CLARIF-i-stable/utils.py
+++ CLARIF-i-stable/utils.py
@@ -1,9 +1,12 @@
 import random
+import itertools as it
 
 from .api.TestCase import TestCase
 from .api.State import State
 
 def generate_sorting_test_case(n: int) -> TestCase:
-    keys = ( f"k{i}" for i in range(n) )
-    start_state = State(dict(zip(keys, random.shuffle())))
-    goal_state = State(dict(zip(keys, range(n))))
+    keys_s = ( f"k{i}" for i in range(n) )
+    keys_g = it.tee(keys_s)
+    start_state = State(dict(zip(keys_s, random.shuffle(list(range(n))))))
+    goal_state = State(dict(zip(keys_g, range(n))))
+    
