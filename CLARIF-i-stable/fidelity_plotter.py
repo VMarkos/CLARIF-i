b0c10db7a5fc69c01fb7dbf3b2e430b7e0577f88
bill <vassileiosmarkos@gmail.com>
2025-09-22 13:13:43 +0300

    Generate different coach types fidelities

--- CLARIF-i-stable/fidelity_plotter.py
+++ CLARIF-i-stable/fidelity_plotter.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 from statistics import mean, stdev
 
 from utils import compute_fidelity, find_bubble_swap_action, find_bubble_partial_swap_action, find_quick_swap_action, find_quick_partial_swap_action
+from api.Coach import Coach, ProactiveCoach, ReflexiveCoach
 from api.State import State
 
 COACH_ACTIONS = {
@@ -19,10 +20,16 @@ COACH_ACTIONS = {
     'qp': find_quick_partial_swap_action,
 }
 
+COACH_TYPES = {
+    'a': Coach,
+    'r': ReflexiveCoach,
+    'p': ProactiveCoach,
+}
+
 COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color']
 MARKERS = ('^', '>', 'v', '<')
 
-CONFIGS = it.product( (True, False), (True, False), COACH_ACTIONS.keys() )
+CONFIGS = filter( lambda c: c[0] or not c[1], it.product( (True, False), (True, False), COACH_ACTIONS.keys(), list(COACH_TYPES.keys())[1:] ) )
 
 CONFIG_COLORS = dict(zip(COACH_ACTIONS.keys(), zip(COLORS, MARKERS)))
 
@@ -57,19 +64,20 @@ def parse_traces(path: str) -> dict[int, list[list[State]]]:
         trace_list.append(traces) # Append last config
     return trace_list
 
-def compute_fidelities(traces: list) -> list:
+def compute_fidelities(traces: list, coach_type: str='r') -> list:
     """ traces as parsed by `parse_traces()` """
     fidelities = dict()
     for ts, config in zip(traces, CONFIGS):
-        if not config[0] and config[1]:
+        if (not config[0] and config[1]) or config[3] != coach_type:
             continue
-        coach_action_fn = COACH_ACTIONS[config[-1]]
+        coach_action_fn = COACH_ACTIONS[config[2]]
         fidelities[config] = { n: [ compute_fidelity(t, coach_action_fn) for t in trs ] for n, trs in ts.items() }
     return fidelities
 
 def plot_fidelities(fidelities: dict, save_path)-> None:
     config_to_str = lambda c: f"{c[2]}, Mem: {'Long' if c[1] else 'Short' if c[0] else 'None'}"
     fig, ax = plt.subplots()
+    ax.set_ylim((-0.05, 1.05))
     handles = []
     for config, fidelity in fidelities.items():
         ns, mean_fs, std_fs = zip(*( (n, mean(fs), stdev(fs)) for n, fs in fidelity.items() ))
@@ -103,6 +111,13 @@ def prepare_parser() -> ArgumentParser:
         default=5,
         help='State size step. Default value: 5',
     )
+    parser.add_argument(
+        '-ct',
+        type=str,
+        default='r',
+        choices=['a', 'r', 'p'],
+        help="Coach type: re{a}ctive, {r}eflexive, {p}roactive. Default value: 'r'",
+    )
     return parser
 
 def main():
@@ -110,16 +125,16 @@ def main():
     RESULTS_PATH = os.path.join(CWD, "raw_results")
     PLOTS_PATH = os.path.join(CWD, "plots")
     parser = prepare_parser()
-    N, r, s = vars(parser.parse_args()).values()
+    N, r, s, ct = vars(parser.parse_args()).values()
     fname = f"fidelity_test_N{N}_reps{r}_step{s}"
-    source_name = fname + ".trace"
+    source_name = fname + "_coaches2.trace"
     res_path = os.path.join(RESULTS_PATH, source_name)
-    fig_name = fname + ".pdf"
+    fig_name = fname + f"_coach_{ct}.pdf"
     fig_path = os.path.join(PLOTS_PATH, fig_name)
     print('Parsing results...')
     traces = parse_traces(res_path)
     print('Magic happens...')
-    fidelities = compute_fidelities(traces)
+    fidelities = compute_fidelities(traces, coach_type=ct)
     print('Creating plot...')
     plot_fidelities(fidelities, fig_path)
     print(f'Plot saved at: {fig_path}')
