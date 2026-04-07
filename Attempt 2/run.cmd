931546f8943279c468f65103c054189257cb6f6a
bill <vassileiosmarkos@gmail.com>
2026-04-07 15:14:49 +0300

    Init LNAI

--- Attempt 2/run.cmd
+++
@@ -1,2 +0,0 @@
-python starter.py
-pause
--- Attempt 2/starter.py
+++
@@ -1,6 +0,0 @@
-# starter.py
-from manager import PreferenceReasoningApp
-
-if __name__ == "__main__":
-    app = PreferenceReasoningApp()
-    app.mainloop()
